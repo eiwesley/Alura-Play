f0e5876afc5f447739408500a1f947d2a90e7334
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-06 17:34:02 -0300

    :sparkles: feat: 2 Busca de vídeos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -23,9 +23,15 @@ async function criaVideos(titulo, descricao, url, imagem){
     return conexaoConvertida;
 }
 
+async function buscaVideo(termoDeBusca){
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json();
 
+    return conexaoConvertida;
+}
 
 export const conectaApi = {
     listaVideos, 
-    criaVideos
+    criaVideos,
+    buscaVideo
 }
